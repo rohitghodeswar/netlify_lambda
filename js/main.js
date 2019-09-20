1cc206fed5b9282d2802021950650e89eb56ddbe
Rohit Ghodeswar <rohit.ghodeswar@hitachiconsulting.com>
2019-09-20 13:12:42 +0530

    minor fixes

--- js/main.js
+++ js/main.js
@@ -4,12 +4,12 @@ const fetchUsers = async () =>
 fetchUsers().then(data => {
   userList = document.querySelector("#users");
 
-  data.forEach(service => {
+  data.forEach(user => {
     const li = document.createElement("li");
     li.className = "list-group-item";
     const link = document.createElement("a");
-    link.appendChild(document.createTextNode(service.name));
-    link.href = service.id;
+    link.appendChild(document.createTextNode(user.name));
+    link.href = user.name;
     link.target = "_blank";
     li.appendChild(link);
     userList.appendChild(li);
